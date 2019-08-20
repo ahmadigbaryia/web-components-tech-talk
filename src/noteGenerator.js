e246f30f5ccc4d3b17807cbfd0af6fd1a95ee731
ahmad.igbariya <ahmad.igbariya@servicenow.com>
2019-08-20 12:33:48 +0300

    cleanning

--- src/noteGenerator.js
+++ src/noteGenerator.js
@@ -5,13 +5,16 @@ const snTemplate = document.getElementById('sticky-note-template');
  */
 export function generateStickyNote({ text = '', id = new Date().getTime() }) {
     const shadowHost = document.createElement('div');
-    shadowHost.setAttribute("id", id);
+    shadowHost.setAttribute("id", `shadow-host-${id}`);
+    
     const shadowRoot = shadowHost.attachShadow({mode: "open"});
+    
     const node = snTemplate.content.cloneNode(true);
     const stickyNote = node.querySelector('#sticky-note-container');
     //we won't have to give unique ID anymore because it's already encapsulated
     //stickyNote.setAttribute('id', `${stickyNote.getAttribute('id')}-${id}`);
     stickyNote.querySelector('.sticky-note-text').textContent = text;
+    
     shadowRoot.appendChild(node);
     document.body.appendChild(shadowHost);
 }
